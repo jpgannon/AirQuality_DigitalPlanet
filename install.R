c7622896091b756731756577b6a9fde2b7bffa28
JP Gannon <46534026+jpgannon@users.noreply.github.com>
2022-02-18 10:22:58 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages("tidyverse", "htmlwidgets", "IRdisplay", "AirSensor")
+install.packages(c("tidyverse", "htmlwidgets", "IRdisplay", "AirSensor"), repos='http://cran.us.r-project.org')
